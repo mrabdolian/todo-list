2251935be2a0798ae3df78eb18da0bfc8f975783
mrabdolian <mr.abdolian@gmail.com>
2016-08-13 06:27:11 +0430

    Create empty template for task list, Improve UI in task list, Add "edit" and "remove" actions for task list

--- js/controllers/task-list.js
+++ js/controllers/task-list.js
@@ -2,22 +2,71 @@ app.controller('TaskListCtrl', ['$scope', '$stateParams', function ($scope, $sta
 
     $scope.tasks = $scope.subCategories[$stateParams.subCategoryId].tasks;
 
+    $scope.emptyEditing = function () {
+        $scope.editing = {
+            key: null,
+            bkpTask: {},
+            error: false
+        };
+    };
+
+    $scope.emptyEditing();
+
     $scope.emptyNewTask = function () {
         $scope.newTask = {
             title: '',
             description: '',
             important: false,
-            dueDate: '',
+            dueDate: null,
             done: false,
-            doneDate: ''
+            doneDate: null
         };
     };
 
     $scope.emptyNewTask();
 
-    $scope.addTask = function () {
-        $scope.tasks.push($scope.newTask);
-        $scope.emptyNewTask();
-    }
+    $scope.focusOnNewTask = function () {
+        $('#newTaskTitle').focus();
+    };
+
+    $scope.submitTask = function () {
+        if ($scope.editing.key !== null) {  // if editing an item...
+            $scope.emptyEditing();
+            $scope.newTask = {};
+
+        } else {
+            $scope.tasks.push($scope.newTask);
+            $scope.emptyNewTask();
+            $scope.focusOnNewTask();
+        }
+    };
+
+    $scope.editTask = function (key) {
+        if ($scope.editing.key !== null) {  // if editing an item...
+            $scope.taskEditCancel();
+        }
+        $scope.editing = {
+            key: angular.copy(key),
+            bkpTask: angular.copy($scope.tasks[key]),
+            error: false
+        };
+        $scope.newTask = $scope.tasks[key];
+        $scope.focusOnNewTask();
+    };
+
+    $scope.taskEditCancel = function () {
+        $scope.tasks[$scope.editing.key] = $scope.editing.bkpTask;
+        $scope.emptyEditing();
+        $scope.newTask = {};
+    };
+
+    $scope.removeTask = function (key) {
+        if ($scope.editing.key === key) {
+            $scope.editing.error = true;
+        }
+        else {
+            $scope.tasks.splice(key, 1);
+        }
+    };
 
 }]);
